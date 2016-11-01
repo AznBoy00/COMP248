9a7ecdaee58f2b555023ed335ff26e582e37dd24
AznBoy00 <Lin_Kevin_1995@hotmail.com>
2016-10-31 23:33:52 -0400

    FIX Q1

--- A3/A3_40002383/src/a3_40002383/A3_Q1_40002383.java
+++ A3/A3_40002383/src/a3_40002383/A3_Q1_40002383.java
@@ -18,100 +18,104 @@ public class A3_Q1_40002383 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         //Variable Declaration
         Scanner input = new Scanner(System.in);
         int nounNum;
         int adjNum;
-        String [] nouns;
-        int [] nounCheck;
-        String [] adj;
-        int [] adjCheck;
-        
-        int randomNoun;
-        int randomAdj;
-        
+        String[] nouns;
+        String[] adj;
+
+        int runCount = 0;
+        int maxRunCount = 0;
+        int lineIndex = 0;
         char repeat;
-        
+        String temp;
+        int randomNum;
+
         System.out.println("\t----------------------------------------------");
         System.out.println("\tDo you want to write a poem? Lets get started!");
         System.out.println("\t----------------------------------------------");
-        
+
         System.out.println("How many nouns ? min 3");
-        nounNum= input.nextInt();
+        nounNum = input.nextInt();
         while (nounNum < 3) {
             System.out.println("How many nouns ? min 3");
-            nounNum= input.nextInt();
+            nounNum = input.nextInt();
         }
-        nouns = new String [nounNum];
-        nounCheck = new int [nounNum];
-        for (int i = 0; i < nounNum; i++) {
-            nounCheck [i] = 0;
-        }
-        
+        nouns = new String[nounNum];
+
         System.out.println("Enter " + nounNum + " nouns");
         for (int i = 0; i < nounNum; i++) {
-            nouns [i] = input.next();
-        }    
-        
+            nouns[i] = input.next();
+        }
+
         System.out.println("How many adjectives ? min 3");
         adjNum = input.nextInt();
         while (adjNum < 3) {
             System.out.println("How many adjectives ? min 3");
-            adjNum= input.nextInt();
+            adjNum = input.nextInt();
         }
-        adj = new String [adjNum];
-        adjCheck = new int [adjNum];
-        for (int i = 0; i < adjNum; i++) {
-            adjCheck [i] = 0;
-        }
-        
+        adj = new String[adjNum];
+
         System.out.println("Enter " + adjNum + " adjectives");
         for (int i = 0; i < adjNum; i++) {
-            adj [i] = input.next();
+            adj[i] = input.next();
         }
-        
-        System.out.println("\t------------------------------");
-        System.out.println("\t\tHere is my Java POEM!!");
-        System.out.println("\t\t****LOOK AROUND****");
-        System.out.println("\t------------------------------\n");
-        
-        for (int i = 0; i < 9; i++) {    
-            randomAdj = (int)(Math.random()*adjNum);
-            randomNoun = (int)(Math.random()*nounNum);
-            while (adjCheck[randomAdj] == 1) {
-                randomAdj = (int)(Math.random()*adjNum);
-            }
-            while (nounCheck[randomNoun] == 1) {
-                randomNoun = (int)(Math.random()*nounNum);
-            }
-            adjCheck [randomAdj] = 1;
-            nounCheck [randomNoun] = 1;
-            
-            System.out.print("\t");
-            for (int j = 0; j < i; j++) {
-                System.out.print("\t");
-            }
-            System.out.println(adj[randomAdj].toUpperCase() + " " + nouns[randomNoun].toUpperCase());
+        //getting true length
+        if (nouns.length > adj.length) {
+            maxRunCount = (adj.length) / 3;
+        } else if (adj.length >= nouns.length) {
+            maxRunCount = (nouns.length) / 3;
         }
-        
-        System.out.print("\n\nAnother Poem (y/n)? ");
-        repeat = (input.next().charAt(0));
-        
-        while (repeat == 'y') {
+        //shuffling arrays
+        for (int i = 0; i < nouns.length; i++) {
+            randomNum = (int) (Math.random() * nouns.length);
+            temp = nouns[i];
+            nouns[i] = nouns[randomNum];
+            nouns[randomNum] = temp;
+        }
+        for (int i = 0; i < adj.length; i++) {
+            randomNum = (int) (Math.random() * adj.length);
+            temp = adj[i];
+            adj[i] = adj[randomNum];
+            adj[randomNum] = temp;
+        }
+
+        do {
             System.out.println("\t------------------------------");
             System.out.println("\t\tHere is my Java POEM!!");
             System.out.println("\t\t****LOOK AROUND****");
             System.out.println("\t------------------------------\n");
-            
-            System.out.println("\n\t" + adj[(int)(Math.random()*adjNum)].toUpperCase() + " " + nouns[(int)(Math.random()*nounNum)].toUpperCase());
-            System.out.println("\t\t" + adj[(int)(Math.random()*adjNum)].toUpperCase() + " " + nouns[(int)(Math.random()*nounNum)].toUpperCase());
-            System.out.println("\t\t\t" + adj[(int)(Math.random()*adjNum)].toUpperCase() + " " + nouns[(int)(Math.random()*nounNum)].toUpperCase());
-            
+
+            for (int i = 0; i < 3; i++) {
+                System.out.print("\t");
+                for (int j = 0; j < i; j++) {
+                    System.out.print("\t");
+                }
+                System.out.println(adj[lineIndex].toUpperCase() + " " + nouns[lineIndex].toUpperCase());
+                lineIndex++;
+            }
             System.out.print("\n\nAnother Poem (y/n)? ");
             repeat = (input.next().charAt(0));
-        }       
-        
+            runCount++;
+            //shuffling arrays
+            if (runCount == maxRunCount) {
+                runCount = 0;
+                lineIndex = 0;
+                for (int i = 0; i < nouns.length; i++) {
+                    randomNum = (int) (Math.random() * nouns.length);
+                    temp = nouns[i];
+                    nouns[i] = nouns[randomNum];
+                    nouns[randomNum] = temp;
+                }
+                for (int i = 0; i < adj.length; i++) {
+                    randomNum = (int) (Math.random() * adj.length);
+                    temp = adj[i];
+                    adj[i] = adj[randomNum];
+                    adj[randomNum] = temp;
+                }
+            }
+        } while (repeat == 'y');
     }
-    
 }
